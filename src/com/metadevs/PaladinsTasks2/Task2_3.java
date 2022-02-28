55eff32775987a449a3179802784b9d26a53fb66
GeorgyMiroshnikov <gmiroshnikov@meta-devs.io>
2022-02-28 23:10:03 +0500

    внёс изменения согласно комментариям в ПР

--- src/com/metadevs/PaladinsTasks2/Task2_3.java
+++ src/com/metadevs/PaladinsTasks2/Task2_3.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Task2_3 {
     public static void main(String[] args) {
-
         Scanner ans = new Scanner(System.in);
         System.out.print("Что говорит кошка Жука?: ");
         String intAnswer = ans.nextLine();
